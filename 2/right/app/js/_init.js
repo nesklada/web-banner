42d10acd431497c40c880e29e3d16a2151f7c405
Stan <nesklada.gov@gmail.com>
2023-05-27 13:02:33 +0300

    [right] designed

---
+++ 2/right/app/js/_init.js
@@ -0,0 +1,5 @@
+(function () {
+    document.body.addEventListener('click', () => {
+        // window.open('https://www.google.com/search?q=Left banner');
+    });
+})();
---
+++ 2/right/app/js/custom.js
@@ -0,0 +1,33 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./app/js/_init.js":
+/*!*************************!*\
+  !*** ./app/js/_init.js ***!
+  \*************************/
+/***/ (() => {
+
+eval("(function () {\n  document.body.addEventListener('click', function () {// window.open('https://www.google.com/search?q=Left banner');\n  });\n})();\n\n//# sourceURL=webpack://myproject/./app/js/_init.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./app/js/_init.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=custom.js.map
